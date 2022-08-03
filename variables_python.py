adf6489bc23c672a2cc2b16676f09bf3e9ce1778
DaanJe2002 <110546251+DaanJe2002@users.noreply.github.com>
2022-08-03 16:40:32 +0200

    Add files via upload

---
+++ variables_python.py
@@ -0,0 +1,20 @@
+#first_name = "Dawid"
+#last_name = "Jessa"
+#full_name = first_name+' '+ last_name
+#print("Siema "+full_name)
+
+#age = 20
+#age = age + 1
+#print(age)
+#print(type(age))
+#print("twoj wiek wynosi:"+ str(age))
+
+#height = 250.5
+#print(height)
+#print(type(height))
+#print("Twoj wzrost wynosi:"+ str(height)+'cm')
+
+human = False
+print('Czy jestes czlowiekiem:'+str(human))
+print(type(human))
+
