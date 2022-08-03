e78ecbc002509accdb88417f08e4ef12095fe3ac
DaanJe2002 <110546251+DaanJe2002@users.noreply.github.com>
2022-08-03 16:41:35 +0200

    Add files via upload

---
+++ multiply_assigment_python.py
@@ -0,0 +1,9 @@
+name = "dawid"
+age = 20
+atrakcyjny = True
+
+name, age, atrakcyjny = "dawid",21,True
+print(name,age,atrakcyjny)
+
+Spongbob = pratcik = sandy = suid = 30
+print(pratcik,suid,sandy,Spongbob)
