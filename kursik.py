ad04b51617d73bd54d9610b7945aa6a14d3da6f2
DaanJe2002 <110546251+DaanJe2002@users.noreply.github.com>
2022-08-03 16:36:08 +0200

    multiply assigment

--- kursik.py
+++ kursik.py
@@ -1,19 +1,9 @@
-#first_name = "Dawid"
-#last_name = "Jessa"
-#full_name = first_name+' '+ last_name
-#print("Siema "+full_name)
+name = "dawid"
+age = 20
+atrakcyjny = True
 
-#age = 20
-#age = age + 1
-#print(age)
-#print(type(age))
-#print("twoj wiek wynosi:"+ str(age))
+name, age, atrakcyjny = "dawid",21,True
+print(name,age,atrakcyjny)
 
-#height = 250.5
-#print(height)
-#print(type(height))
-#print("Twoj wzrost wynosi:"+ str(height)+'cm')
-
-human = False
-print('Czy jestes czlowiekiem:'+str(human))
-print(type(human))
+Spongbob = pratcik = sandy = suid = 30
+print(pratcik,suid,sandy,Spongbob)
