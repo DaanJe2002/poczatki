73c74505c4905b865ebb8d1a88059ed4fb7fee08
DaanJe2002 <110546251+DaanJe2002@users.noreply.github.com>
2022-08-06 14:42:46 +0200

    Add files via upload

---
+++ list_python.py
@@ -0,0 +1,15 @@
+food = ['pizza','hamburger','hotdog','spaghetti']
+
+#print(food[0])
+
+food[0] = 'sushi'
+#food.append('icecream')
+#food.remove('hotdog')
+#food.pop()
+#food.insert(0,'cake')
+#food.sort()
+food.clear()
+#print(food[0])
+
+for i in food:
+    print(i)
