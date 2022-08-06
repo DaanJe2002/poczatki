c80facf3e52b66fe1b33aa8b6854446253ed52be
DaanJe2002 <110546251+DaanJe2002@users.noreply.github.com>
2022-08-06 14:58:08 +0200

    Add files via upload

---
+++ set_python.py
@@ -0,0 +1,15 @@
+costam = {'fork','spoon','knife'}
+cos2 = {'miska','talerz','lyzka','knife'}
+
+
+#costam.add('napkins')
+#costam.remove()
+#costam.clear()
+#costam.update(cos2)
+#print(costam)
+
+print(cos2.difference(costam))
+print(cos2.intersection(costam))
+
+#for x in costam:
+ #       print(x)
