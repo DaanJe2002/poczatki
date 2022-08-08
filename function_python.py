ba79f2a4f7251539df294491515167c1915fd06a
DaanJe2002 <110546251+DaanJe2002@users.noreply.github.com>
2022-08-08 15:36:49 +0200

    Add files via upload

---
+++ function_python.py
@@ -0,0 +1,8 @@
+#name = input("Podaj swoje imie kolego:")
+def reapet(first_name,last_name,wiek):
+    print('siema '+first_name)
+    print('milego dnia panie '+last_name)
+    print('masz '+str(wiek)+' lat')
+
+reapet('Dawid','Jessa',20)
+
