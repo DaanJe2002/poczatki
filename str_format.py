f550d7136cf83a5de1c54ec51cd6f898753ffd57
DaanJe2002 <110546251+DaanJe2002@users.noreply.github.com>
2022-08-09 16:45:09 +0200

    Add files via upload

---
+++ str_format.py
@@ -0,0 +1,31 @@
+
+
+animal = 'krowa'
+item = 'ksiezyc'
+
+#print('The '+animal+' jump ober the '+item)
+#print('the {} jump over {}'.format(animal,item))
+#print('the {1} jump over {0}'.format(animal,item))
+#print('the {item} jump over {item}'.format(animal = 'cow',item = 'moon'))
+
+
+#text = 'The {} jump over the {}'
+#print(text.format(animal,item))
+
+name = 'dawid'
+
+#print('siema moje imie to {}'.format(name))
+#print('siema moje imie to {:10}.nice to meet you '.format(name))
+#print('siema moje imie to {:>10}.nice to meet you '.format(name))
+#print('siema moje imie to {:^10}.nice to meet you '.format(name))
+#print('siema moje imie to {:10}.nice to meet you '.format(name))
+
+
+number = 1000
+
+print('numer powyzej wynosi {:}'.format(number))
+print('numer powyzej wynosi {:,}'.format(number))
+print('numer powyzej wynosi {:b}'.format(number))
+print('numer powyzej wynosi {:o}'.format(number))
+print('numer powyzej wynosi {:X}'.format(number))
+print('numer powyzej wynosi {:E}'.format(number))
